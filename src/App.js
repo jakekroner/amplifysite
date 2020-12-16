36cb452b11f09a01b8cd83895f7eea9b3b05a5e1
Jake Kroner <jakekroner@Jake-Kroner.local>
2020-12-15 19:09:19 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+      <div className = "App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+      </div>
+    );
 }
 
 export default App;
